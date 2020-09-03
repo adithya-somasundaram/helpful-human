8917c21cc3043213948b0e1f2c8f4548ed59d186
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-09-02 17:08:39 -0700

    adding data

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import * as colors from '../db.json'
 
-function ListView(){
-    const test = {
-        position : 'absolute',
-        paddingLeft : '30%'
+import '../style/ListView.css'
+
+class ListView extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            total : 0,
+            current : 0,
+            pages : [],
+            color_objs: [[]],
+            test : {
+                position : 'absolute',
+                paddingLeft : '30%'
+            }
+        }
     }
-    return(
-        <div style={test}>
-            Welcome
-        </div>
-    )
+
+    componentDidMount(){
+        var temp = []
+        var temp2 = []
+        colors.data.map((block) => {
+            temp.push(<button onClick={() =>{
+                this.setState({
+                    current : block.id-1
+                })
+            }
+            }>{block.id}</button>)
+            var temp3 = []
+            block.colors.map((current) => {
+            temp3.push(<button>{current}</button>)
+            })
+            temp2.push(temp3)
+        })
+        this.setState({
+            pages: temp,
+            color_objs : temp2
+        })
+    }
+
+
+    render(){
+        return(
+            <div className="test">
+                <div>
+                    Welcome
+                </div>
+                {this.state.color_objs[this.state.current]}
+                <br/>
+                {this.state.pages}
+            </div>
+        )
+    }
+    
 }
 
 export default ListView;
