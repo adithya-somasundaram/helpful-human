76cd875cab8d1cc6ba83c14ea29bdc124066ab75
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-09-03 18:25:17 -0700

    fixed sidebar bug

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 import '../style/Sidebar.css'
 
-function Sidebar() {
-    return (
-        <div className="bar">
-            <button className="rand-button">Random Color</button>
-            <br/>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Red
-            </Link>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Orange
-            </Link>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Yellow
-            </Link>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Green
-            </Link>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Blue
-            </Link>
-            <br/>
-            <Link className="color-link" to='/detail-view'>
-                Purple
-            </Link>
-        </div>
-    );
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className="bar">
+                <button className="rand-button" onClick={() => { console.log('ok') }}>Random Color</button>
+                <br />
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Red
+                </Link>
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Orange
+                </Link>
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Yellow
+                </Link>
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Green
+                </Link>
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Blue
+                </Link>
+                <br />
+                <Link className="color-link" to='/detail-view'>
+                    Purple
+                </Link>
+            </div>
+        );
+    }
+
 }
 
 export default Sidebar;
