0b533061f24e3f2fc98506445e7e9512b4dcc4bd
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-09-05 17:28:36 -0700

    comments

--- src/connection.js
+++ src/connection.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
+// open db connection
 let data = new sqlite3.Database('./src/colors.db', sqlite3.OPEN_READONLY, (err)=>{
     if(err){
         return console.error(err.message)
@@ -7,15 +8,20 @@ let data = new sqlite3.Database('./src/colors.db', sqlite3.OPEN_READONLY, (err)=
     console.log('connected')
 })
 
+// d holds all data, temp holds data for 1 row
 var d = []
 var temp = []
 
+// row_count is colors per/page, index holds number of pages
 var row_count = 12
 var index = 1
 
-data.all(`SELECT * FROM colors`, [], (err, res)=>{
+// query the data from the db
+data.all(`SELECT * FROM colors`, [], (res)=>{
     for(let i = 0; i < res.length; i++){
         temp.push(res[i].name)
+
+        // push and reset temp if row overflows
         if (temp.length >= row_count){
             d.push({id: String(index), colors: temp})
             temp = []
@@ -23,20 +29,20 @@ data.all(`SELECT * FROM colors`, [], (err, res)=>{
         }
     }
 
+    // add remaining colors
     if(temp.length > 0){
         d.push({id: String(index), colors: temp})
     }
 
-
-
-    var test = {data:d}
-
+    // store data in json format
+    var j = {data:d}
     var fs = require('fs')
-    fs.writeFile('./src/db.json', JSON.stringify(test), function(err){
+    fs.writeFile('./src/db.json', JSON.stringify(j), function(err){
         if (err) throw err;
     })
 })
 
+// close db connection
 data.close((err)=>{
     if(err){
         return console.error(err.message)
--- src/db_populate.py
+++ src/db_populate.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# connect/create db color
 connection = sqlite3.connect('src/colors.db')
 cur = connection.cursor()
 
+# reset colors table
 cur.execute('DROP TABLE IF EXISTS colors')
 cur.execute('CREATE TABLE IF NOT EXISTS colors(name TEXT)')
 
@@ -16,10 +18,6 @@ for i in range(100):
     connection.commit()
     root += offset
 
-cur.execute('SELECT * FROM colors')
-d = cur.fetchall()
-print(d)
-
-
+# close db connection
 cur.close()
 connection.close()
